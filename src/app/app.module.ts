e1ca36b9fa7b882bc7729c06b913ccb0ae6162b7
Leandro-Michail-Krikis <leo1996.dasilva@gmail.com>
2023-01-16 22:05:07 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,33 @@ import { ContentComponent } from './components/content/content.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FiltroComponent } from './components/filtro/filtro.component';
-
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ContentComponent,
     FooterComponent,
-    FiltroComponent
+    FiltroComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([
+      {path: 'home', redirectTo: 'home/todos'},
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home/todos'
+      },
+      {path: 'home/todos', component: HomeComponent},
+      {path: 'home/nao-lidos', component: HomeComponent},
+      {path: 'home/lidos', component: HomeComponent},
+      {path: 'sobre', component: AboutComponent}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
